693bfe7882a927367fe50ba0660b6d9405412060
Caspian Ahlberg <caspianahlberg@gmail.com>
2024-04-17 15:15:49 -0400

    For the Twilio message deletion script, make the curl deletion command its own spawned process for the sake of speed, and make it read the body and date sent correctly

--- assets/delete_twilio_msgs.bash
+++ assets/delete_twilio_msgs.bash
@@ -11,10 +11,13 @@ msgs_json=`curl -s -X GET $request_url -u $account_sid:$auth_token`
 
 # TODO: how do I pass many messages to delete at once?
 
-for ((i=0; i<$deletion_count; i++))
-do
-	msg_fields=`jq -r ".messages[$i] | .uri, .body, .from, .date_sent" <<< $msgs_json`
-	read uri body from date_sent <<< $msg_fields
-	curl -X DELETE "$base_request_url/$uri" -u $account_sid:$auth_token
+for ((i=0; i<$deletion_count; i++)) do
+	# TODO: how to read these 4 with just 1 `jq` command?
+	message_info=`jq -r ".messages[$i]" <<< $msgs_json`
+	read uri from <<< `jq -r ".uri, .from" <<< $message_info`
+	body=`jq -r ".body" <<< $message_info`
+	date_sent=`jq -r ".date_sent" <<< $message_info`
+
+	curl -X DELETE "$base_request_url/$uri" -u $account_sid:$auth_token &
 	echo "Deleted message: '$body' (from '$from', on '$date_sent')\n---"
 done
