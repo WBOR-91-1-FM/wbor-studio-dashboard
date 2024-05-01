c9e9488f4ca97ffa5cac31b4c5ce710087e1b9c7
Caspian Ahlberg <caspianahlberg@gmail.com>
2024-05-01 15:18:06 -0400

    Add some thread task TODOs, and do some minor reformatting there too

--- src/utility_types/thread_task.rs
+++ src/utility_types/thread_task.rs
@@ -5,8 +5,18 @@ use crate::utility_types::generic_result::{GenericResult, MaybeError};
 
 //////////
 
-/* TODO: can I avoid using threads here, and just have an async function
-that runs in short bursts when you call `update` (or work with coroutines somehow)? */
+/* TODO:
+- Can I avoid using threads here, and just have an async function that runs
+in short bursts when you call `update` (or work with coroutines somehow)?
+I can maybe use the `park_timeout`, `sleep`, `yield_now`, or `sleep_until` for that...
+
+- Is there a way to keep the thread alive after finishing (continually sending),
+and not doing channel reopening, thread restarting, and all of that? Perhaps put
+the computation loop in the thread computer itself, possibly.
+
+- Allow for thread joining (would be useful for Twilio) (but this is assuming that I don't do the thread-always-alive idea)
+*/
+
 pub trait Updatable {
 	type Param: Clone + Send;
 	fn update(&mut self, param: &Self::Param) -> MaybeError;
@@ -22,9 +32,9 @@ impl<T: Updatable + Clone + Send + 'static> ContinuallyUpdated<T> {
 	/* TODO: can I make this lazy, so that it only starts working once I call `update`,
 	and possibly only update again after a successful `update` call (with a pause?) */
 	pub fn new(data: &T, param: <T as Updatable>::Param, name: &'static str) -> Self {
-		let mut cloned_data = data.clone();
-
 		let (thread_sender, thread_receiver) = mpsc::channel();
+
+		let mut cloned_data = data.clone();
 
 		let mut computer = move || {
 			match cloned_data.update(&param) {
@@ -49,11 +59,7 @@ impl<T: Updatable + Clone + Send + 'static> ContinuallyUpdated<T> {
 		match self.thread_receiver.try_recv() {
 			Ok(Ok(new_data)) => *self = Self::new(&new_data, param.clone(), self.name),
 			Ok(Err(err)) => error = Some(err.into()),
-
-			Err(mpsc::TryRecvError::Empty) => {
-				// Waiting for a response...
-			},
-
+			Err(mpsc::TryRecvError::Empty) => {}, // Waiting for a response...
 			Err(err) => error = Some(err.to_string())
 		}
 
