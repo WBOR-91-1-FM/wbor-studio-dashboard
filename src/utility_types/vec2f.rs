9405d080464f9c39b46cebf111425565ef46fcef
Caspian (Fedora Linux) <caspianahlberg@gmail.com>
2024-05-06 16:07:10 -0400

    Add a TODO

--- src/utility_types/vec2f.rs
+++ src/utility_types/vec2f.rs
@@ -1,5 +1,7 @@
 use std::ops;
 
+/* TODO: could I wrap this in a struct type?
+That would then allow for things like closure under multiplication */
 type Component = f32;
 
 // A 0-1 normalized floating-point vec2
