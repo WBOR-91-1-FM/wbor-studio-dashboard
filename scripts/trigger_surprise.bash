4f27ed452e5284676799fadec9aee2a5ba2a8793
Caspian (Fedora Linux) <caspianahlberg@gmail.com>
2024-06-15 16:12:40 -0700

    Change how some stuff is printed

--- scripts/trigger_surprise.bash
+++ scripts/trigger_surprise.bash
@@ -2,14 +2,18 @@
 
 # Script param: surprise name.
 
+log() {
+	echo ">>> $1"
+}
+
 fail() {
-	echo "$1"
+	log "$1"
 	exit 1
 }
 
 path="$1"
 
-echo "Note: if the dashboard is not in focus, this script may hang until it becomes so."
+log "Note: if the dashboard is not in focus, this script may hang until it becomes so."
 
 if [[ "$path" == "" ]]; then
 	fail "Please provide a surprise path (in the format of \"assets/<surprise_name>\")!"
@@ -17,4 +21,4 @@ fi
 
 printf "$path" | nc -U /tmp/surprises_wbor_studio_dashboard.sock || fail "Could not send the path to the dashboard's socket!"
 
-echo ">>> Sent the surprise to the dashboard. Check the dashboard logs to see that the surprise was received."
+log "Sent the surprise to the dashboard. Check the dashboard logs to see that the surprise was received."
