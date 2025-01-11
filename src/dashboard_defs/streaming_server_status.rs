1a32679f18c534286754f8c6689a989c83a3dea4
Caspian Ahlberg <caspianahlberg@gmail.com>
2025-01-11 16:28:50 -0500

    Now displaying the status of the online streaming server in the little pop-up error window. If it's down, something will display there.

---
+++ src/dashboard_defs/streaming_server_status.rs
@@ -0,0 +1,69 @@
+use crate::{
+	request,
+	dashboard_defs::shared_window_state::SharedWindowState,
+
+	window_tree::{
+		Window,
+		WindowContents,
+		WindowUpdaterParams
+	},
+
+	utility_types::{
+		vec2f::Vec2f,
+		generic_result::*,
+		update_rate::UpdateRate,
+		dynamic_optional::DynamicOptional,
+		continually_updated::{ContinuallyUpdated, Updatable}
+	}
+};
+
+#[derive(Clone)]
+struct ServerStatusChecker {
+	num_retries: u8
+}
+
+impl ServerStatusChecker {
+	fn new(num_retries: u8) -> Self {
+		Self {num_retries}
+	}
+}
+
+impl Updatable for ServerStatusChecker {
+	type Param = ();
+
+	async fn update(&mut self, _: &Self::Param) -> MaybeError {
+		for _ in 0..self.num_retries {
+			match request::get("https://azura.wbor.org/api/nowplaying/2").await {
+				Ok(_) => return Ok(()),
+				Err(_) => continue
+			}
+		}
+
+		error_msg!("Could not reach the streaming server!")
+	}
+}
+
+fn server_status_updater_fn(params: WindowUpdaterParams) -> MaybeError {
+	let inner_shared_state = params.shared_window_state.get_mut::<SharedWindowState>();
+	let individual_window_state = params.window.get_state_mut::<ContinuallyUpdated<ServerStatusChecker>>();
+	individual_window_state.update(&(), &mut inner_shared_state.error_state)?;
+	Ok(())
+}
+
+pub async fn make_streaming_server_status_window(ping_rate: UpdateRate, num_retries: u8) -> Window {
+	let pinger_updater = ContinuallyUpdated::new(
+		&ServerStatusChecker::new(num_retries),
+		&(),
+		"the online streaming server"
+	).await;
+
+	Window::new(
+		Some((server_status_updater_fn, ping_rate)),
+		DynamicOptional::new(pinger_updater),
+		WindowContents::Nothing,
+		None,
+		Vec2f::ZERO,
+		Vec2f::ZERO,
+		None
+	)
+}
